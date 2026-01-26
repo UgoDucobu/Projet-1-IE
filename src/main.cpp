0e96ae3e5984fb21acaf6894f2d21d572bf16371
UgoDucobu <ducobu.ugo@gmail.com>
2026-01-26 11:22:52 +0100

    LCD, bouton, potentiomètre

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,34 @@ void setup() {
   lcd.begin(16, 2, LCD_5x8DOTS, Wire1);
   lcd.printf("Trieur de balles");
 
+  //verification terminal
+  Serial.printf("verification");
+
+  //Initialisation bouton
+  #define bp2 12
+
+  // Initialisation du port série pour debug
+  Serial.begin(115200);
+  delay(1000);
+
+  // Configuration de la broche bouton en entrée avec résistance interne pull-up
+  pinMode(bp2, INPUT_PULLUP);
+
+  Serial.println();
 }
 
 void loop() {
+  if (digitalRead(bp2) == 0) {
+    lcd.setRGB(255, 0, 0);
+    lcd.setCursor(4, 1);
+    lcd.printf("etat = 1");
+    Serial.printf("etat = 1\n");
+  } else {
+    lcd.setRGB(0, 0, 255);
+    lcd.setCursor(4, 1);
+    lcd.printf("etat = 0");
+    Serial.printf("etat = 0\n");
+  }
 
+  delay(200); // Anti-rebond simple
 }
