6921758d8b941412cecf9322369469b0f034d2a2
UgoDucobu <ducobu.ugo@gmail.com>
2026-01-26 11:23:33 +0100

    LCD, boutons, potentiomètre

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "rgb_lcd.h"
 
 rgb_lcd lcd;
-
+float tens;
 void setup() {
   // Initialise la liaison avec le terminal
   Serial.begin(115200);
@@ -17,6 +17,7 @@ void setup() {
 
   //Initialisation bouton
   #define bp2 12
+  #define pot 33
 
   // Initialisation du port série pour debug
   Serial.begin(115200);
@@ -26,20 +27,25 @@ void setup() {
   pinMode(bp2, INPUT_PULLUP);
 
   Serial.println();
+  
 }
 
 void loop() {
   if (digitalRead(bp2) == 0) {
     lcd.setRGB(255, 0, 0);
-    lcd.setCursor(4, 1);
+    lcd.setCursor(0, 1);
     lcd.printf("etat = 1");
     Serial.printf("etat = 1\n");
   } else {
     lcd.setRGB(0, 0, 255);
-    lcd.setCursor(4, 1);
+    lcd.setCursor(0, 1);
     lcd.printf("etat = 0");
     Serial.printf("etat = 0\n");
   }
+  tens = analogRead(pot) * 3.3 / 4096;
+  lcd.setCursor(10, 1);
+  lcd.printf("%.1f V", tens);
+  Serial.printf("%.1f V", tens);
 
   delay(200); // Anti-rebond simple
 }
