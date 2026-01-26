2d76fb34423e0bcfbdd5e203c940d6e91e91eb03
UgoDucobu <ducobu.ugo@gmail.com>
2026-01-26 12:33:04 +0100

    hacheur

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,20 @@
 #include "rgb_lcd.h"
 
 rgb_lcd lcd;
+ //Initialisation bouton
+  #define bp0 0
+  #define bp1 2
+  #define bp2 12
+  #define pot 33
+  #define sens 26
+int etat = 0;
 float tens;
+float duty;
+  //Initialisation PWM
+const int pwmPin = 27; // broche utilisée
+const int pwmChannel = 0; // Canal LEDC 
+const int pwmFreq = 25000; // 25 kHz 
+const int pwmResolution = 11; // 11 bits → valeurs de 0 à 2047
 void setup() {
   // Initialise la liaison avec le terminal
   Serial.begin(115200);
@@ -10,14 +23,12 @@ void setup() {
   // Initialise l'écran LCD
   Wire1.setPins(15, 5);
   lcd.begin(16, 2, LCD_5x8DOTS, Wire1);
-  lcd.printf("Trieur de balles");
+  //lcd.printf("Trieur de balles");
 
   //verification terminal
   Serial.printf("verification");
 
-  //Initialisation bouton
-  #define bp2 12
-  #define pot 33
+ 
 
   // Initialisation du port série pour debug
   Serial.begin(115200);
@@ -27,11 +38,21 @@ void setup() {
   pinMode(bp2, INPUT_PULLUP);
 
   Serial.println();
-  
+
+
+    // Configuration du PWM 
+    ledcSetup(pwmChannel, pwmFreq, pwmResolution); 
+    
+    // Attache la broche au canal PWM 
+    ledcAttachPin(pwmPin, pwmChannel); 
+    
+    // Duty cycle = 0,25 → 25 % 
+    
+
 }
 
 void loop() {
-  if (digitalRead(bp2) == 0) {
+  /*if (digitalRead(bp2) == 0) {
     lcd.setRGB(255, 0, 0);
     lcd.setCursor(0, 1);
     lcd.printf("etat = 1");
@@ -41,11 +62,38 @@ void loop() {
     lcd.setCursor(0, 1);
     lcd.printf("etat = 0");
     Serial.printf("etat = 0\n");
+  }*/
+  
+  switch(etat) {
+    case 0:
+      lcd.clear();
+      duty = 0;
+      lcd.printf("stop");
+      if(digitalRead(bp0) == 0) { etat = 1; }
+      else if(digitalRead(bp1) != 0) { etat = 2; } 
+      break;
+    case 1:
+      lcd.clear();
+      digitalWrite(sens, 1);
+      duty = analogRead(pot) * 0.99 / 4096 * ((1 << pwmResolution) - 1); 
+      lcd.printf("horaire");
+      if(digitalRead(bp2) == 0) { etat = 0; }
+      break;
+    case 2:
+      lcd.clear();
+      digitalWrite(sens, 1);
+      duty = analogRead(pot) * 0.99 / 4096 * ((1 << pwmResolution) - 1); 
+      lcd.printf("anti-horaire");
+      if(digitalRead(bp2) == 0) { etat = 0; }
+      break;
   }
+  ledcWrite(pwmChannel, duty);
+  
   tens = analogRead(pot) * 3.3 / 4096;
   lcd.setCursor(10, 1);
   lcd.printf("%.1f V", tens);
   Serial.printf("%.1f V", tens);
-
+  lcd.setCursor(0, 0);
+ 
   delay(200); // Anti-rebond simple
 }
